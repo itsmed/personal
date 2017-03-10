5405736a3eef3ed26c43c32660ca1572148016e1
DejonGill <dejonjgill@gmail.com>
2017-03-10 09:43:24 -0800

    added firebase, google oatuh

--- src/Application.js
+++ src/Application.js
@@ -9,14 +9,32 @@ import './Application.css';
 class Application extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((currentUser) => {
+      console.log('auth changed', currentUser);
+      this.setState({ currentUser });
+    });
   }
 
   render() {
+    const { currentUser } = this.state;
+
     return (
       <div className="Application">
         <header className="Application--header">
           <h1>Lunch Rush</h1>
         </header>
+        <div>
+          { /* alternative to a ternary, single version! */ } 
+          { !currentUser && <SignIn /> }
+          { currentUser && <CurrentUser user={currentUser} /> }
+        </div>
       </div>
     );
   }
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
 
-const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  storageBucket: '',
-  messagingSenderId: ''
+var config = {
+  apiKey: "AIzaSyDyLAYmkg6DPCKDyjAPo6LbZe1j81qMGhg",
+  authDomain: "lunch-rush-5c8d2.firebaseapp.com",
+  databaseURL: "https://lunch-rush-5c8d2.firebaseio.com",
+  storageBucket: "lunch-rush-5c8d2.appspot.com",
+  messagingSenderId: "444200460907"
 };
 
 firebase.initializeApp(config);
