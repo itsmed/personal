a78d36de826cc53c80ad575dcf9ff832a56442d8
DejonGill <dejonjgill@gmail.com>
2017-03-14 13:54:40 -0700

    Added logic to mark as done with line through

--- src/todoList/TodoList.js
+++ src/todoList/TodoList.js
@@ -2,9 +2,25 @@ import React, { Component } from 'react';
 
 import TodoItem from '../todoItem/TodoItem';
 
+
 class TodoList extends Component {
   constructor(props) {
     super(props);
+
+    this.handleTodoClick = this.handleTodoClick.bind(this);
+  }
+
+  handleTodoClick(ref) {
+    const ele = this.refs[ref];
+    const blue = {
+      color: 'blue',
+    };
+
+    const black = {
+      color: 'black',
+    };
+    console.log('clicked', ele.style);
+    ele.style.textDecoration = ele.style.textDecoration === '' ? 'line-through' : 'none';
   }
 
   render() {
@@ -13,7 +29,7 @@ class TodoList extends Component {
     return <div>
       <ul>
       {
-        todos.map(todo => <li key={ todo } ref={ todo }> <TodoItem item={ todo } /> </li> )
+        todos.map(todo => <li key={ todo } onClick={ () => this.handleTodoClick(todo) } ref={ todo} > <TodoItem item={ todo } /> </li> )
       }
       </ul>
     </div>;
