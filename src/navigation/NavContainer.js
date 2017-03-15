838e45847a6db9da5c213b05e8f60b9062053875
DejonGill <dejonjgill@gmail.com>
2017-03-15 12:10:01 -0700

    init

---
+++ src/navigation/NavContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NavContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <nav>
+      
+    </nav>;
+  }
+}
+
+export default NavContainer;
